2b3a6f4b3816427a492f31f9f167f4ce2ef5c1b5
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-01 15:38:47 -0300

    Add leadedProducts

--- lib/views/products_overview_screem.dart
+++ lib/views/products_overview_screem.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
 
 class ProductOverviewScren extends StatelessWidget {
+  final List<Product> leadedProducts = DUMMY_PRODUCTS;
   @override
   Widget build(BuildContext context) {
     return Container();
