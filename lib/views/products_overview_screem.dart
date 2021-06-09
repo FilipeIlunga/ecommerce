ed667b7eb08a86c04aa8f29f406c8cfb9f1028dc
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-09 17:28:22 -0300

    Add filterOptions

--- lib/views/products_overview_screem.dart
+++ lib/views/products_overview_screem.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:shop/widgets/product_Grid.dart';
 
+enum FilterOptions { Favorite, All }
+
 class ProductOverviewScren extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Minha Loja"),
-      ),
+      appBar: AppBar(title: Text("Minha Loja"), actions: <Widget>[
+        PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {},
+            itemBuilder: (_) => [
+                  PopupMenuItem(
+                    child: Text('Todos'),
+                    value: FilterOptions.All,
+                  ),
+                  PopupMenuItem(
+                      child: Text('Somente Favoritos'),
+                      value: FilterOptions.Favorite)
+                ])
+      ]),
       body: ProductGrid(),
     );
   }
