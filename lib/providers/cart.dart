cc1260860dca4ac28687864b4a03bad6c64558dd
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-28 16:42:54 -0300

    Add addItem method

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/foundation.dart';
+import 'package:shop/providers/product.dart';
 
 class CartItem {
   final String id;
@@ -17,5 +20,28 @@ class Cart with ChangeNotifier {
   Map<String, CartItem> _items;
   Map<String, CartItem> get item {
     return {..._items};
+  }
+
+  void addItem(Product product) {
+    if (_items.containsKey(product.id)) {
+      _items.update(
+        product.id,
+        (existingItem) => CartItem(
+            id: existingItem.id,
+            title: existingItem.title,
+            quantity: existingItem.quantity + 1,
+            price: existingItem.price),
+      );
+    } else {
+      _items.putIfAbsent(
+        product.id,
+        () => CartItem(
+            id: Random().nextDouble().toString(),
+            title: product.title,
+            quantity: 1,
+            price: product.price),
+      );
+    }
+    notifyListeners();
   }
 }
