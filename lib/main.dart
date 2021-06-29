9c32f031a7e76ccecf55204db72b3acec9cd241b
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-28 23:44:00 -0300

    Add CartScreen route

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:shop/providers/cart.dart';
 import 'package:shop/providers/products.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/views/cartScreen.dart';
 import 'package:shop/views/products_overview_screem.dart';
 
 import 'views/product_detail_screen.dart';
@@ -28,7 +29,8 @@ class MyApp extends StatelessWidget {
             fontFamily: 'Lato',
           ),
           routes: {
-            AppRoutes.PRODUCT_Detail: (context) => ProductDetailScreen()
+            AppRoutes.PRODUCT_Detail: (context) => ProductDetailScreen(),
+            AppRoutes.PRODUCT_CART: (context) => CartScreen(),
           },
           home: ProductOverviewScren()),
     );
