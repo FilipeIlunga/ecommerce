51c89f979da0fe25a1b19d1422eb3402d412409b
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-28 23:53:01 -0300

    Add cartScreen body

--- lib/views/cartScreen.dart
+++ lib/views/cartScreen.dart
@@ -4,8 +4,28 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text("Carrinho"),
-    ));
+      appBar: AppBar(
+        title: Text("Carrinho"),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: EdgeInsets.all(25),
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Text(
+                    "Total",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Chip(label: Text("R\$ 1000"))
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
