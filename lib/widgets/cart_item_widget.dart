3e592bce783588ab9c073693ee233abb4277b93e
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-29 23:03:19 -0300

    Create CartItemWidget

---
+++ lib/widgets/cart_item_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shop/providers/cart.dart';
+
+class CartItemWidget extends StatelessWidget {
+  CartItemWidget(this.cartItem);
+  final CartItem cartItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            title: Text(cartItem.title),
+          ),
+        ));
+  }
+}
