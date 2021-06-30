296cd2a547dfb31ebd44e9c603f5d2fc9f165b6e
Filipe Ilunga <filipeilunga@gmail.com>
2021-06-29 23:42:10 -0300

    Add ListTile

--- lib/widgets/cart_item_widget.dart
+++ lib/widgets/cart_item_widget.dart
@@ -12,8 +12,16 @@ class CartItemWidget extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.all(8),
           child: ListTile(
-            title: Text(cartItem.title),
-          ),
+              leading: CircleAvatar(
+                child: Padding(
+                  padding: EdgeInsets.all(1),
+                  child: FittedBox(child: Text(" ${cartItem.price}")),
+                ),
+              ),
+              title: Text(cartItem.title),
+              subtitle:
+                  Text("Total: R\$ ${cartItem.price * cartItem.quantity}"),
+              trailing: Text("${cartItem.quantity}x")),
         ));
   }
 }
