f19b1438e3b3aac8754a7af716ddeac9d241bc5e
Filipe Ilunga <filipeilunga@gmail.com>
2021-07-09 00:07:30 -0300

    Add appDrawer

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppDrawer extends StatefulWidget {
+  @override
+  _AppDrawerState createState() => _AppDrawerState();
+}
+
+class _AppDrawerState extends State<AppDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
