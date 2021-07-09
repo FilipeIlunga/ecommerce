e44c1858583a53e4758235e8677e9ddcd87a9c66
Filipe Ilunga <filipeilunga@gmail.com>
2021-07-09 00:10:50 -0300

    Add Drawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/utils/app_routes.dart';
 
 class AppDrawer extends StatefulWidget {
   @override
@@ -8,6 +9,28 @@ class AppDrawer extends StatefulWidget {
 class _AppDrawerState extends State<AppDrawer> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Drawer(
+      child: Column(children: [
+        AppBar(title: Text("Bem vindo Usuário")),
+        Divider(),
+        ListTile(
+            leading: Icon(Icons.shop),
+            title: Text("Loja"),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(
+                AppRoutes.HOME,
+              );
+            }),
+        Divider(),
+        ListTile(
+            leading: Icon(Icons.payment),
+            title: Text("Pedidos"),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(
+                AppRoutes.ORDERS,
+              );
+            })
+      ]),
+    );
   }
 }
