1eac4a0fd6208ba3a7fa368d342eace742e52831
Filipe Ilunga <filipeilunga@gmail.com>
2021-07-08 23:01:27 -0300

    Add product_detail_screen style

--- lib/views/product_detail_screen.dart
+++ lib/views/product_detail_screen.dart
@@ -11,6 +11,33 @@ class ProductDetailScreen extends StatelessWidget {
         centerTitle: true,
         title: Text(product.title),
       ),
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 2.5,
+            width: double.infinity,
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text("R\$ ${product.price}",
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              )),
+          SizedBox(height: 10),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            width: double.infinity,
+            child: Text(
+              product.description,
+              textAlign: TextAlign.center,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
